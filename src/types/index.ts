31e35326a097d0f2559599d6a3b79abf12f41521
Aksinya Fedichkina <ukenglish97@yandex.ru>
2025-11-25 17:57:49 +0300

    fix: исправлен TPayment на тип

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,9 @@
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type TPayment = 'card' | 'cash';
 
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
-}
-
-export interface TPayment {
-    payment: 'card' | 'cash';
 }
 
 export interface IProduct {
@@ -23,4 +20,10 @@ export interface IBuyer {
     email: string;
     phone: string;
     address: string;
+}
+
+// раньше зачем-то TPayment сделала интерфейсом, а не типом, писала функцию для проверки
+// хочу оставить ее, вдруг понадобиться когда-нибудь :)
+export function isTPayment(value: string): value is TPayment {
+  return value === 'card' || value === 'cash';
 }
